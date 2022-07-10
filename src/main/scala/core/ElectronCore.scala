c6cdd0ec99b19407e0632c79d0b3f32b1af5c4b6
ysyx_22041381-Qianyu Cheng <qianyu.cheng.chn@gmail.com>
2022-07-11 00:16:26 +0800

    Add 5-stages pipeline, without hazard.

--- src/main/scala/core/ElectronCore.scala
+++ src/main/scala/core/ElectronCore.scala
@@ -13,25 +13,27 @@ class ElectronCore extends Module {
   val fetch = Module(new Fetch)
 
   val i_addr_trans = Module(new AddrTrans)
-  i_addr_trans.io.vaddr := fetch.io.pc
+  i_addr_trans.io.vaddr := fetch.io.npc //pre-IF
   io.pc := fetch.io.pc
 
   val iram = Module(new SinglePortRAM)
   iram.io.en := 1.B
-  iram.io.addr := i_addr_trans.io.paddr(15,2)
+  iram.io.addr := i_addr_trans.io.paddr(15,2) //pre-IF
   iram.io.wen := 0.B
+  iram.io.wdata := DontCare
 
   //ID
+  val br_taken_flush = Wire(Bool())
+  val if_id_reg_instr = Pipe(1.B, Mux(br_taken_flush, "b0000001010_000000000000_00000_00000".U, iram.io.rdata))
+
   val decode = Module(new Decode)
-  decode.io.instr := iram.io.rdata
+  decode.io.instr := if_id_reg_instr.bits
 
   val rf = Module(new GR)
   rf.io.raddr1.valid := 1.B
   rf.io.raddr2.valid := 1.B
-  rf.io.waddr.valid := !(decode.io.ctrl.reg2mem || decode.io.ctrl.branch)
   rf.io.raddr1.bits := decode.io.ctrl.rj
   rf.io.raddr2.bits := Mux(decode.io.ctrl.reg2mem || decode.io.ctrl.branch, decode.io.ctrl.rd, decode.io.ctrl.rk)
-  rf.io.waddr.bits := Mux(decode.io.ctrl.bl, 1.U(5.W), decode.io.ctrl.rd)
 
   val br_taken = decode.io.ctrl.branch && ((rf.io.rdata1 === rf.io.rdata2) ^ decode.io.ctrl.bne)
   val offs16 = Cat(Fill(14, decode.io.ctrl.imm26(25)), decode.io.ctrl.imm26(25, 10), 0.U(2.W))
@@ -42,26 +44,51 @@ class ElectronCore extends Module {
   fetch.io.base.valid := decode.io.ctrl.jirl
   fetch.io.base.bits := rf.io.rdata1
 
+  br_taken_flush := decode.io.ctrl.bl || br_taken || decode.io.ctrl.jirl
+
   //EXE
+  val id_exe_reg_ctrl = Pipe(1.B, decode.io.ctrl)
+  val id_exe_reg_rdata1 = Pipe(1.B, rf.io.rdata1)
+  val id_exe_reg_rdata2 = Pipe(1.B, rf.io.rdata2)
+  val id_exe_reg_rf_wen = Pipe(1.B, !(decode.io.ctrl.reg2mem || decode.io.ctrl.branch))
+  val id_exe_reg_rf_waddr = Pipe(1.B, Mux(decode.io.ctrl.bl, 1.U(5.W), decode.io.ctrl.rd))
+
   val alu = Module(new ALU)
-  alu.io.ctrl := decode.io.ctrl.alu_ctrl
-  val si12 = Cat(Fill(20, decode.io.ctrl.imm26(21)), decode.io.ctrl.imm26(21,10))
-  alu.io.src1 := rf.io.rdata1
-  alu.io.src2 := Mux(decode.io.ctrl.sel_src2, si12, rf.io.rdata2)
+  alu.io.ctrl := id_exe_reg_ctrl.bits.alu_ctrl
+  val si12 = Cat(Fill(20, id_exe_reg_ctrl.bits.imm26(21)), id_exe_reg_ctrl.bits.imm26(21,10))
+  alu.io.src1 := id_exe_reg_rdata1.bits
+  alu.io.src2 := Mux(id_exe_reg_ctrl.bits.sel_src2, si12, id_exe_reg_rdata2.bits)
 
   //MEM
-  val d_addr_trans = Module(new AddrTrans)
-  d_addr_trans.io.vaddr := alu.io.result
+  val exe_mem_reg_alu_result = Pipe(1.B, alu.io.result)
+  val exe_mem_reg_mem2reg = Pipe(1.B, id_exe_reg_ctrl.bits.mem2reg)
+  val exe_mem_reg_lui = Pipe(1.B, id_exe_reg_ctrl.bits.lui)
+  val exe_mem_reg_imm26 = Pipe(1.B, id_exe_reg_ctrl.bits.imm26)
+  val exe_mem_reg_rf_wen = Pipe(id_exe_reg_rf_wen)
+  val exe_mem_reg_rf_waddr = Pipe(id_exe_reg_rf_waddr)
 
-  val dram = Module(new SinglePortRAM)
-  dram.io.en := decode.io.ctrl.mem2reg || decode.io.ctrl.reg2mem
-  dram.io.addr := d_addr_trans.io.paddr
-  dram.io.wen := decode.io.ctrl.reg2mem
-  dram.io.wdata := rf.io.rdata2
+  val d_addr_trans = Module(new AddrTrans)
+  d_addr_trans.io.vaddr := alu.io.result // pre-MEM
+
+  val dram = Module(new SinglePortSyncRAM)
+  dram.io.en := id_exe_reg_ctrl.bits.mem2reg || id_exe_reg_ctrl.bits.reg2mem //pre-MEM
+  dram.io.addr := d_addr_trans.io.paddr(15, 2) //pre-MEM
+  dram.io.wen := id_exe_reg_ctrl.bits.reg2mem //pre-MEM
+  dram.io.wdata := id_exe_reg_rdata2.bits //pre-MEM
 
   //WB
-  val si20 = Cat(decode.io.ctrl.imm26(24,5), 0.U(12.W))
-  rf.io.wdata := Mux(decode.io.ctrl.lui, si20, Mux(decode.io.ctrl.mem2reg, dram.io.wdata, alu.io.result))
+  val mem_wb_reg_rdata = Pipe(1.B, dram.io.rdata)
+  val mem_wb_reg_alu_result = Pipe(exe_mem_reg_alu_result)
+  val mem_wb_reg_mem2reg = Pipe(exe_mem_reg_mem2reg)
+  val mem_wb_reg_lui = Pipe(exe_mem_reg_lui)
+  val mem_wb_reg_imm26 = Pipe(exe_mem_reg_imm26)
+  val mem_wb_reg_rf_wen = Pipe(exe_mem_reg_rf_wen)
+  val mem_wb_reg_rf_waddr = Pipe(exe_mem_reg_rf_waddr)
+
+  val si20 = Cat(mem_wb_reg_imm26.bits(24,5), 0.U(12.W))
+  rf.io.waddr.valid := mem_wb_reg_rf_wen.bits
+  rf.io.waddr.bits := mem_wb_reg_rf_waddr.bits
+  rf.io.wdata := Mux(mem_wb_reg_lui.bits, si20, Mux(mem_wb_reg_mem2reg.bits, mem_wb_reg_rdata.bits, mem_wb_reg_alu_result.bits))
 }
 
 object Generator extends App{
--- src/main/scala/core/Fetch.scala
+++ src/main/scala/core/Fetch.scala
@@ -6,15 +6,18 @@ import chisel3.util._
 class Fetch extends Module{
   val io = IO(new Bundle() {
     val pc = Output(UInt(32.W))
+    val npc = Output(UInt(32.W))
     val offs = Flipped(Valid(UInt(32.W)))
     val base = Flipped(Valid(UInt(32.W)))
   })
 
-  val pc = RegInit(0x1c000000.U(32.W))
+  val pc = RegInit(0x1bffffff.U(32.W))
 
   val npc = Mux(io.base.valid, io.base.bits, pc) + Mux(io.offs.valid, io.offs.bits, 4.U)
 
   pc := npc
 
   io.pc := pc
+
+  io.npc := npc
 }
