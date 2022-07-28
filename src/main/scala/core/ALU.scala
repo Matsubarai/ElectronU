ed13d9ad55a891a11dd3d4cb2d67b95bca0ac472
ysyx_22041381-Qianyu Cheng <qianyu.cheng.chn@gmail.com>
2022-07-28 17:17:17 +0800

    Refactor Decode, MemPort and ALU.

--- src/main/scala/core/ALU.scala
+++ src/main/scala/core/ALU.scala
@@ -7,20 +7,19 @@ object ALU
 {
   val SZ_ALU_CTRL = 4
   def ALU_ADD  = "b0000".U
-  def ALU_SLL  = "b0001".U
-  def ALU_SEQ  = "b0010".U
-  def ALU_SNE  = "b0011".U
-  def ALU_XOR  = "b0100".U
-  def ALU_SRL  = "b0101".U
-  def ALU_OR   = "b0110".U
-  def ALU_AND  = "b0111".U
-  def ALU_NOR  = "b1001".U
-  def ALU_SUB  = "b1010".U
-  def ALU_SRA  = "b1011".U
-  def ALU_SLT  = "b1100".U
-  def ALU_SGE  = "b1101".U
-  def ALU_SLTU = "b1110".U
-  def ALU_SGEU = "b1111".U
+  def ALU_SUB  = "b0001".U
+
+  def ALU_SLT  = "b1001".U
+  def ALU_SLTU = "b1011".U
+
+  def ALU_NOR  = "b0100".U
+  def ALU_AND  = "b0101".U
+  def ALU_OR   = "b0111".U
+  def ALU_XOR  = "b0110".U
+
+  def ALU_SLL  = "b1100".U
+  def ALU_SRL  = "b1110".U
+  def ALU_SRA  = "b1111".U
 }
 
 import ALU._
@@ -33,30 +32,21 @@ class ALU extends Module {
     val result = Output(UInt(32.W))
   })
 
-  val is_sub = io.ctrl(3) //sub, slt, sltu, sra, nor
+  val is_sub = io.ctrl(0) //sub, slt, sltu, sra
   val src2_inv = Mux(is_sub, ~io.src2, io.src2)
   val adder_result = io.src1 + src2_inv + is_sub
 
-  val is_sltu = io.ctrl(1)
+  val is_sltu = io.ctrl(1) //sltu, srl, sra
   val slt_result = Mux(io.src1(31) === io.src2(31), adder_result(31), Mux(is_sltu, io.src2(31), io.src1(31)))
 
   val shamt = io.src2(4, 0)
-  val shift_src = Mux(io.ctrl === ALU_SRL || io.ctrl === ALU_SRA, io.src1, Reverse(io.src1))
+  val shift_src = Mux(is_sltu, io.src1, Reverse(io.src1))
   val shift_result_r = (Cat(is_sub & shift_src(31), shift_src).asSInt >> shamt)(31, 0)
   val shift_result_l = Reverse(shift_result_r)
-  val shift_result = Mux(io.ctrl === ALU_SRL || io.ctrl === ALU_SRA, shift_result_r, shift_result_l)
+  val shift_result = Mux(is_sltu, shift_result_r, shift_result_l)
 
-  val logic_result = Mux(io.ctrl === ALU_XOR || io.ctrl === ALU_OR, io.src1 ^ io.src2, 0.U) |
-    Mux(io.ctrl === ALU_OR || io.ctrl === ALU_AND, io.src1 & io.src2, (~(io.src1 | io.src2)).asUInt)
+  val or_result = io.src1 | io.src2
+  val logic_result = Mux1H(UIntToOH(io.ctrl(1, 0)), Seq(~or_result, io.src1 ^ io.src2, io.src1 & io.src2, or_result))
 
-  io.result := DontCare
-  when(io.ctrl === ALU_ADD || io.ctrl === ALU_SUB){
-    io.result := adder_result
-  }.elsewhen(io.ctrl === ALU_SLT || io.ctrl === ALU_SLTU){
-    io.result := Cat(0.U(31.W), slt_result)
-  }.elsewhen(io.ctrl === ALU_SLL || io.ctrl === ALU_SRL || io.ctrl === ALU_SRA){
-    io.result := shift_result
-  }.elsewhen(io.ctrl === ALU_AND || io.ctrl === ALU_OR || io.ctrl === ALU_XOR || io.ctrl === ALU_NOR){
-    io.result := logic_result
-  }
+  io.result := Mux1H(UIntToOH(io.ctrl(3, 2)), Seq(adder_result, logic_result, Cat(0.U(31.W), slt_result), shift_result))
 }
--- src/main/scala/core/Decode.scala
+++ src/main/scala/core/Decode.scala
@@ -21,55 +21,46 @@ class CtrlSignals extends Bundle{
   val b = Bool()
   val bl = Bool()
   val jirl = Bool()
+  val brk = Bool()
+  val sys = Bool()
+  val csr = Bool()
   val imm26 = UInt(26.W)
 }
 
 class Decode extends Module {
   val io = IO(new Bundle() {
     val instr = Input(UInt(32.W))
-    val ctrl = Output(new CtrlSignals)
+    val ctrl = Valid(new CtrlSignals)
   })
 
-  io.ctrl.rj := io.instr(9, 5)
-  io.ctrl.rk := io.instr(14, 10)
-  io.ctrl.rd := io.instr(4, 0)
-  io.ctrl.imm26 := io.instr(25, 0)
+  io.ctrl.valid := Seq(ADD_W, ADDI_W, SUB_W,
+    LD_BU, LD_HU, LD_B, LD_H, LD_W, ST_W, ST_H, ST_B,
+    BEQ, BNE, BLT, BGE, BLTU, BGEU, JIRL, B, BL,
+    SLT, SLTU, SLTUI, SLTI,
+    SLL_W, SRL_W, SRA_W, SLLI_W, SRLI_W, SRAI_W,
+    MUL_W, MULH_W, MULH_WU, DIV_W, DIV_WU, MOD_W, MOD_WU,
+    BREAK, SYSCALL, LU12I_W, PCADDU12I,
+    NOR, AND, OR, XOR, ANDI, ORI, XORI).map(io.instr === _).reduce(_ || _)
 
-  io.ctrl.alu_ctrl := DontCare
-  when(io.instr === ADD_W || io.instr === ADDI_W ||
-    io.instr === LD_W || io.instr === ST_W ||
-    io.instr === LD_BU || io.instr === LD_B || io.instr === ST_B ||
-    io.instr === LD_HU || io.instr === LD_H || io.instr === ST_H ||
-    io.instr === JIRL || io.instr === BL || io.instr === PCADDU12I || io.instr === LU12I_W){
-    io.ctrl.alu_ctrl := ALU_ADD
-  }.elsewhen(io.instr === SUB_W){
-    io.ctrl.alu_ctrl := ALU_SUB
-  }.elsewhen(io.instr === SLT || io.instr === SLTI){
-    io.ctrl.alu_ctrl := ALU_SLT
-  }.elsewhen(io.instr === SLTU || io.instr === SLTUI){
-    io.ctrl.alu_ctrl := ALU_SLTU
-  }.elsewhen(io.instr === SLL_W || io.instr === SLLI_W){
-    io.ctrl.alu_ctrl := ALU_SLL
-  }.elsewhen(io.instr === SRL_W || io.instr === SRLI_W){
-    io.ctrl.alu_ctrl := ALU_SRL
-  }.elsewhen(io.instr === SRA_W || io.instr === SRAI_W){
-    io.ctrl.alu_ctrl := ALU_SRA
-  }.elsewhen(io.instr === AND){
-    io.ctrl.alu_ctrl := ALU_AND
-  }.elsewhen(io.instr === OR){
-    io.ctrl.alu_ctrl := ALU_OR
-  }.elsewhen(io.instr === XOR){
-    io.ctrl.alu_ctrl := ALU_XOR
-  }.elsewhen(io.instr === NOR){
-    io.ctrl.alu_ctrl := ALU_NOR
-  }
+  io.ctrl.bits.rj := io.instr(9, 5)
+  io.ctrl.bits.rk := io.instr(14, 10)
+  io.ctrl.bits.rd := io.instr(4, 0)
+  io.ctrl.bits.imm26 := io.instr(25, 0)
 
-  io.ctrl.muldiv_ctrl := Cat(io.instr === DIV_W || io.instr === MOD_W || io.instr === DIV_WU || io.instr === MOD_WU,
+  io.ctrl.bits.alu_ctrl := Mux1H(Cat(Seq(io.instr === ADD_W || io.instr === ADDI_W ||
+    io.instr === LD_W || io.instr === ST_W || io.instr === LD_BU || io.instr === LD_B || io.instr === ST_B ||
+    io.instr === LD_HU || io.instr === LD_H || io.instr === ST_H || io.instr === JIRL || io.instr === BL ||
+    io.instr === PCADDU12I || io.instr === LU12I_W, io.instr === SUB_W, io.instr === SLT || io.instr === SLTI,
+    io.instr === SLTU || io.instr === SLTUI, io.instr === SLL_W || io.instr === SLLI_W,
+    io.instr === SRL_W || io.instr === SRLI_W, io.instr === SRA_W || io.instr === SRAI_W,
+    io.instr === NOR, io.instr === AND, io.instr === OR, io.instr === XOR)),
+    Seq(ALU_ADD, ALU_SUB, ALU_SLT, ALU_SLTU, ALU_SLL, ALU_SRL, ALU_SRA, ALU_NOR, ALU_AND, ALU_OR, ALU_XOR))
+
+  io.ctrl.bits.muldiv_ctrl := Cat(io.instr === DIV_W || io.instr === MOD_W || io.instr === DIV_WU || io.instr === MOD_WU,
     io.instr === MULH_WU || io.instr === MULH_W || io.instr === DIV_W || io.instr === DIV_WU,
     io.instr === MULH_W || io.instr === MUL_W || io.instr === DIV_W || io.instr === MOD_W)
 
-  io.ctrl.sel_src := Cat(io.instr === LU12I_W,
-    io.instr === PCADDU12I,
+  io.ctrl.bits.sel_src := Cat(io.instr === LU12I_W, io.instr === PCADDU12I,
     io.instr === JIRL || io.instr === BL,
     io.instr === SLLI_W || io.instr === SRLI_W || io.instr === SRAI_W,
     io.instr === ANDI || io.instr === ORI || io.instr === XORI,
@@ -78,22 +69,29 @@ class Decode extends Module {
       io.instr === LD_HU || io.instr === LD_H || io.instr === ST_H ||
       io.instr === LD_W || io.instr === ST_W )
 
-  io.ctrl.mem2reg := io.instr === LD_W || io.instr === LD_B || io.instr === LD_H || io.instr === LD_BU || io.instr === LD_HU
+  io.ctrl.bits.mem2reg := io.instr === LD_W || io.instr === LD_B || io.instr === LD_H ||
+    io.instr === LD_BU || io.instr === LD_HU
 
-  io.ctrl.reg2mem := io.instr === ST_W || io.instr === ST_B || io.instr === ST_H
+  io.ctrl.bits.reg2mem := io.instr === ST_W || io.instr === ST_B || io.instr === ST_H
 
-  io.ctrl.bhw := Cat(io.instr === LD_B || io.instr === LD_H,
+  io.ctrl.bits.bhw := Cat(io.instr === LD_B || io.instr === LD_H,
     io.instr === LD_W || io.instr === ST_W,
     io.instr === LD_H || io.instr === LD_HU || io.instr === ST_H)
 
-  io.ctrl.br_comp_ctrl := Cat(io.instr === BNE || io.instr === BGE || io.instr === BGEU,
+  io.ctrl.bits.br_comp_ctrl := Cat(io.instr === BNE || io.instr === BGE || io.instr === BGEU,
     io.instr === BLT || io.instr === BGE || io.instr === BLTU || io.instr === BGEU,
     io.instr === BEQ || io.instr === BNE || io.instr === BLT || io.instr === BGE)
 
-  io.ctrl.b := io.instr === B
+  io.ctrl.bits.b := io.instr === B
 
-  io.ctrl.bl := io.instr === BL
+  io.ctrl.bits.bl := io.instr === BL
 
-  io.ctrl.jirl := io.instr === JIRL
+  io.ctrl.bits.jirl := io.instr === JIRL
+
+  io.ctrl.bits.brk := io.instr === BREAK
+
+  io.ctrl.bits.sys := io.instr === SYSCALL
+
+  io.ctrl.bits.csr := io.instr === CSR
 
 }
